ec05df12a9f9d02d0807ce32d1a906a28e0201c4
SteffenDiehl <Steffen.Diehl@rdf.nuernberg.de>
2023-01-11 13:09:24 +0100

    test

--- Car.py
+++ Car.py
@@ -2,30 +2,42 @@ import time
 import random
 import pygame
 from pygame.locals import *
+pygame.init()
 
-carspawntime = random.randint(5, 10)
+carspawntime = random.randint(3, 5)
 carsinlot = []
 start = time.time()
 carcolours =[]
 extras = ['family', 'handycaped']
 carcounter = 1
-carsize = (100, 50)
+carsize = (60, 30)
 price = 8 # Preis pro h in €
 simend = time.time() + 3* 60
+zeittraffer = 0
 parkinglot = []
 revenue = 0
+hins = 3600 #StundeinSekunde(3600)
+minparkdauer = 10 #900 = 15 min in s
+maxparkdauer = 30 #43200 = 12h in s
 
-reihen = 3
-spalten = 6
-for r in range(reihen):
-    for s in range(spalten):
+maxreihen = 3
+maxspalten = 6
+for r in range(maxreihen):
+    for s in range(maxspalten):
         parkinglot.append((r, s))
 lotcount = len(parkinglot)
+print(lotcount)
 
-dpx = 600
-dpy = 400
-reihenabstand = 30
-platzabstand = 15
+dpx = 1000
+dpy = 800
+reihenabstand = 100
+platzabstand = 25
+parkplatzlaenge = 50
+einfahrtslaenge = 50
+xeinfahrt = 50
+yeinfahrt = 300
+xausfahrt = 600
+yausfahrt = 300
 grey = (170, 170, 170)
 screen = pygame.display.set_mode((dpx, dpy))
 pygame.display.set_caption('First drive')
@@ -44,7 +56,7 @@ class cars:
         self.extra = extra
 
 def spawncar():
-    timer = random.randint(20, 30) #(900, 43200)  # 15min - 12h in s
+    timer = random.randint(minparkdauer, maxparkdauer)
     starttime = time.time() # Zeitpunkt einfahrt
     endtime = starttime + timer # Zeitpunkt ausfahrt
     carcolour = random.choice(carcolours)  # zufällige autofarbe
@@ -62,46 +74,74 @@ def situation():
         extra = 'none'
     return extra
 
-def parkcar(car):
+def parkcar(car): # das auto parken
+    pygame.display.update()
     reihe , spalte = parkinglot[car.lotnumber]
-    x = 0
-    y = 0
+    x = xeinfahrt
+    y = yeinfahrt
     thiscar = car.colour
     screen.blit(thiscar, (x, y))
-    x += 20
+    x += einfahrtslaenge
     screen.blit(thiscar, (x, y))
     thiscar = pygame.transform.rotate(thiscar, -90)
-    for i in range(reihe):
-        y += reihenabstand
+    ydrive = reihenabstand * reihe + y
+    xdrive = platzabstand * spalte + x
+    while y < ydrive:
+        y += 1
         screen.blit(thiscar, (x, y))
         pygame.display.update()
     thiscar = pygame.transform.rotate(thiscar, 90)
-    for i in range(spalte):
-        x += platzabstand
+    while x < xdrive:
+        x += 1
         screen.blit(thiscar, (x, y))
         pygame.display.update()
     thiscar = pygame.transform.rotate(thiscar, -90)
-    y += 5
+    y += parkplatzlaenge
     screen.blit(thiscar, (x, y))
     pygame.display.update()
+    car.colour = thiscar
+    return (x,y)
 
 def getcarout(car):
-    print('tbd')
-    newrevenue = pay(car, revenue)
-    deletecar(car)
-    return newrevenue
+    reihe, spalte = parkinglot[car.lotnumber]
+    thiscar = car.colour
+    y = reihenabstand * reihe + yeinfahrt
+    x = platzabstand * spalte + xeinfahrt + einfahrtslaenge
+    screen.blit(thiscar, (x, y))
+    pygame.display.update()
+    y -= parkplatzlaenge
+    screen.blit(thiscar, (x, y))
+    pygame.display.update()
+    thiscar = pygame.transform.rotate(thiscar, 90)
+    screen.blit(thiscar, (x, y))
+    pygame.display.update()
+    while x < xausfahrt:
+        x += 1
+        screen.blit(thiscar, (x, y))
+        pygame.display.update()
+    while y > yausfahrt:
+        y -= 1
+        screen.blit(thiscar, (x, y))
+        pygame.display.update()
+    thiscar = pygame.transform.rotate(thiscar, 90)
+    x += einfahrtslaenge
+    screen.blit(thiscar, (x, y))
+    pygame.display.update()
+    return (x, y)
 
-def pay(car, newrevenue):
-    hours = car.cartimer//3600
-    if hours < car.cartimer/3600:
+def pay(car, oldrevenue):
+    hours = car.cartimer//hins
+    if hours < car.cartimer/hins:
         hours +=1
     amounttopay = hours * price
-    print(f'You have to pay: {amounttopay}')
-    newrevenue += amounttopay
-    print(f'Your revenue is: {newrevenue}')
+    print(f'You have to pay: {amounttopay} €')
+    newrevenue = oldrevenue + amounttopay
+    print(f'Your revenue is: {newrevenue} €')
     return newrevenue
 def deletecar(car):
+    carsinlot.remove(car)
     print('tbd', car)
+    return
 
 running = True
 while running == True:
@@ -117,15 +157,17 @@ while running == True:
             spawncar()
             carcounter += 1
             car = carsinlot[-1]
-            parkcar(car)
-    for car in carsinlot:
-        print(car.cartimer, car.lotnumber, car.colour, car.extra)
+            screen.blit(car.colour, parkcar(car))
+    for i in carsinlot:
+        print(i.cartimer, i.lotnumber, i.colour, i.extra)
         currenttime = time.time()
-        if currenttime > car.exittime:
-            revenue = getcarout(car)
+        if currenttime > i.exittime:
+            screen.blit(i.colour, getcarout(i))
+            pay(i, revenue)
+            deletecar(i)
     if simend == time.time():
         break
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-    pygame.display.update()
+    #pygame.display.update()
